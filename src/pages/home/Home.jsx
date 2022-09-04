c1241a0f011ac236d0ac72c61f1a55e1b11d44e7
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-09-04 16:02:56 +0530

    Initial commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <div className='mt-10'>
+            <h1 className='text-5xl font-bold p-5'>Products</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      white: "#ffffff",
+      offWhite: "#f7f7f7",
+      gray: "#969191",
+      blue: "#001EB9",
+      black: "#162427",
+    }
+  },
+  plugins: [],
+}
