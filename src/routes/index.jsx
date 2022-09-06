df81cc51af8e0b99268e7120936bf367443c4bd8
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-09-06 15:47:17 +0530

    Implement new route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddProduct from "../pages/AddProduct/AddProduct";
 import EditProduct from "../pages/EditProduct/EditProduct";
+import FavoriteProduct from "../pages/FavoriteProducts/FavoriteProduct";
 import Home from "../pages/home/Home";
 
 export const PageRoute = () => {
@@ -11,6 +12,7 @@ export const PageRoute = () => {
           <Route path="/" element={<Home />} />
           <Route path="/add" element={<AddProduct />} />
           <Route path="/edit/:id" element={<EditProduct />} />
+          <Route path="/fav" element={<FavoriteProduct />} />
         </Routes>
       </BrowserRouter>
     </div>
