12c8d788cd310c618564f9ab2eaa0e318f96b4a1
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-09-06 15:46:55 +0530

    Implement new components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,23 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Header from "../../components/header/Header";
-import { getAllProducts, deleteProduct } from "../../actions/productActions";
+import { getAllProducts, deleteProduct, updateProduct } from "../../actions/productActions";
 import editIcon from "../../Assets/edit-icon.svg";
 import deleteIcon from "../../Assets/delete-icon.svg";
 import starIcon from "../../Assets/star.svg";
 import staredIcon from "../../Assets/starred.svg";
 import { useNavigate } from "react-router-dom";
+import SearchIcon from '@mui/icons-material/Search';
 
 const Home = () => {
+
   const dispatch = useDispatch();
   const allProductsDetails = useSelector((state) => state.allProductsDetails);
   const { loading, error, allProducts } = allProductsDetails;
 
-  const delete_Product = useSelector((state) => state.deleteProduct);
-  const { Dleteloading, DeleteError, success } = delete_Product;
+  const productDelete = useSelector((state) => state.deleteProduct);
+  const { Dleteloading, DeleteError, success } = productDelete;
+
+
 
   const navigation = useNavigate();
-
   const handleUpdate = (
     id,
     name,
@@ -36,22 +39,66 @@ const Home = () => {
   const handleDelete = (id) => {
     if (window.confirm("Are you sure")) {
       dispatch(deleteProduct(id));
+      if (success) {
+        console.log("ok");
+        navigation("/");
+        window.location.reload();
+      }
     }
   };
+
+  const handleFavorite = (id, name, price, description, image, sku, quantity, isFavorite) => {
+    if (isFavorite) {
+      isFavorite = false;
+    } else {
+      isFavorite = true;
+    }
+    console.log(isFavorite);
+    dispatch(updateProduct(id, name, price, description, image, sku, quantity, isFavorite));
+    if (allProducts) {
+      navigation("/");
+      window.location.reload();
+    }
+  }
+
+  const handleProductAdd = () => {
+    navigation("/add");
+    console.log("ok");
+  }
+
+  const GotoFavorites = () => {
+    navigation("/fav");
+  }
 
   useEffect(() => {
     dispatch(getAllProducts());
   }, [dispatch]);
 
-  useEffect(() => {}, [allProductsDetails]);
   return (
     <div>
       <Header />
       <div className="mt-10">
-        <h1 className="text-5xl font-bold p-5">Products</h1>
+        <h1 className="text-5xl font-bold pt-5 pl-20">PRODUCTS</h1>
       </div>
-      <div>
-        <table className="w-full pt-4">
+      <div className="px-20 mt-8 flex justify-between">
+        <div className="relative">
+          <form action="">
+          <input
+              type="text"
+              placeholder="Search the products"
+                className="bg-offWhite w-96 rounded-3xl focus:outline-none pl-4 py-3"
+                name="SKU"
+          />
+            <button className="bg-blue text-white px-4 py-3 rounded-3xl absolute left-[280px]"><span className="flex"><SearchIcon/>Search</span></button>
+          </form>
+        </div>
+        <div className="flex mr-10">
+          <button className="bg-blue text-white rounded-md px-10 py-2" onClick={handleProductAdd}>New Product</button>
+          <button className="bg-white outline rounded-md px-2 ml-5 outline-blue" onClick={GotoFavorites}><img src={staredIcon} alt="star"/></button>
+        </div>
+      </div>
+      <div className="px-28">
+        <table className="w-full mt-8">
           <thead className="">
             <tr>
               <th className="p-3 font-bold text-blue">SKU</th>
@@ -68,10 +115,10 @@ const Home = () => {
                   <div className="pl-20">{product.sku}</div>
                 </td>
                 <td>
-                  <div>{product.name}</div>
+                  <div>{product.image}</div>
                 </td>
                 <td>
-                  <div>{product.image}</div>
+                  <div>{product.name}</div>
                 </td>
                 <td>
                   <div>{product.price}</div>
@@ -99,7 +146,25 @@ const Home = () => {
                       alt="delete"
                       onClick={(e) => handleDelete(product._id)}
                     />
-                    <img src={starIcon} alt="star" />
+                    {product.isFavorite ? (
+                      <img src={staredIcon} alt="star" onClick={(e) => handleFavorite(product._id,
+                        product.name,
+                        product.price,
+                        product.description,
+                        product.image,
+                        product.sku,
+                        product.quantity,
+                        product.isFavorite)} />
+                    ) : (
+                    <img src={starIcon} alt="star" onClick={(e) => handleFavorite(product._id,
+                          product.name,
+                          product.price,
+                          product.description,
+                          product.image,
+                          product.sku,
+                          product.quantity,
+                          product.isFavorite)} />
+                    )}
                   </div>
                 </td>
               </tr>
